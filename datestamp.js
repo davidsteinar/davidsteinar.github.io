6fdc6b476aa299d5953f1076fce4d272a59b7ad7
dsteinar <davidsteinar@gmail.com>
2017-05-18 01:52:17 +0200

    bla

--- datestamp.js
+++ datestamp.js
@@ -3,4 +3,4 @@ month = test.getMonth()
 month = (month * 1) + 1
 day = test.getDate()
 year = test.getFullYear()
-document.write(" ",month,"/",day,"/",year," ")
+document.write(" ", month, "/", day, "/", year, " ")
