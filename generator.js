9bebb5487cf992aa381a0f4fab9d02c0cfc35092
dsteinar <davidsteinar@gmail.com>
2018-09-09 21:05:36 +0200

    added 2.5 grams

--- generator.js
+++ generator.js
@@ -24,8 +24,8 @@ function change_color(){
 
 function get_model(){
   var lang = sidulang.value;
-  var N = siduN.value;
   var modeltype = sidumodel.value;
+  var N = siduN.value;
   
   var model_url = "https://raw.githubusercontent.com/davidsteinar/nynefni/master/models/" + lang + "_" + modeltype + "_" + N +"gram.json";
   var names_url = "https://raw.githubusercontent.com/davidsteinar/nynefni/master/data/names/" + lang + "_" + modeltype + ".json";
@@ -46,7 +46,15 @@ function generate_nafn(){
   var existing_names = JSON.parse(sessionStorage.getItem('existingnames'));
   
   var startkey = startofname.value;
-  var N = Number(siduN.value);
+  var N;
+  
+  if(Number(siduN.value) === 23){
+    N = 3;
+  }
+  else{
+    N =  Number(siduN.value);
+  }
+
   var start = " ".repeat(N) + startkey;
   var initkey = start.slice(start.length-N,start.length);
   var iteration = 0;
@@ -80,7 +88,6 @@ function generate_nafn(){
     
     if(existing_names.includes(nafn) === false && generated_names.includes(nafn) === false){
       generated_names.push(nafn);
-      console.log(generated_names)
       sidunafn.textContent = nafn;
       sidunafn.style.visibility = 'visible';
       break;
