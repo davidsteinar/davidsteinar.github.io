a6aa01d8bbe312a4923dff7e5bc02e6fec56737c
davidsteinar <davidsteinar@gmail.com>
2020-01-25 20:03:38 +0000

    update selection input behaviour

--- generator.js
+++ generator.js
@@ -8,9 +8,28 @@ var sidumodel = document.getElementById("modeltype");
 var siduunique = document.getElementById("unique");
 
 siduN.addEventListener('change', get_model);
-sidulang.addEventListener('change', get_model);
+sidulang.addEventListener('change', validate_input_get_model);
 sidumodel.addEventListener('change', get_model);
 generate.addEventListener('click', generate_nafn);
+
+
+function validate_input_get_model(){
+  var lang = sidulang.value;
+  var not_icelandic_types = document.getElementsByClassName("onlyIcelandic");
+
+  if(lang == 'IS'){
+    for (var i = 0; i < not_icelandic_types.length; i++){
+      not_icelandic_types[i].style.display = 'block';
+    }
+  }
+  else {
+    sidumodel.value = "male";
+    for (var i = 0; i < not_icelandic_types.length; i++){
+      not_icelandic_types[i].style.display = 'none';
+    }
+  }
+  get_model();
+}
 
 function get_model(){
   var lang = sidulang.value;
@@ -32,6 +51,11 @@ function get_model(){
 var generated_names = [];
 
 function generate_nafn(){
+  if(sessionStorage.getItem('model') == null){
+    sidunafn.textContent = "Veldu fyrst tegund";
+    sidunafn.style.visibility = 'visible';
+    return;
+  }
   var model = JSON.parse(sessionStorage.getItem('model'));
   var existing_names = JSON.parse(sessionStorage.getItem('existingnames'));
   
@@ -77,6 +101,11 @@ function generate_nafn(){
     }
     
     if(existing_names.includes(nafn) === false && generated_names.includes(nafn) === false){
+      if(nafn.includes("undefined")){
+        sidunafn.textContent = "Ekkert nafn fannst";
+        sidunafn.style.visibility = 'visible';
+        return;
+      }
       generated_names.push(nafn);
       sidunafn.textContent = nafn;
       sidunafn.style.visibility = 'visible';
